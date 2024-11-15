ac0ba2140458fd39d4d57ae769b9f3ae9647a4de
Clément Doumouro <clement.doumouro@gmail.com>
2024-11-15 16:32:43 +0100

    fix: add PYTHON task group to task

--- datashare_spacy_worker/app.py
+++ datashare_spacy_worker/app.py
@@ -1,13 +1,15 @@
 from icij_worker import AsyncApp
+from icij_worker.app import TaskGroup
 from icij_worker.typing_ import RateProgress
 
 from datashare_spacy_worker.tasks import spacy_ner_task as spacy_ner_
 from datashare_spacy_worker.tasks.dependencies import APP_LIFESPAN_DEPS
 
 app = AsyncApp("spacy", dependencies=APP_LIFESPAN_DEPS)
+PYTHON_TASK_GROUP = "PYTHON"
 
 
-@app.task(name="spacy-ner")
+@app.task(name="spacy-ner", group=TaskGroup(name=PYTHON_TASK_GROUP))
 async def spacy_ner(
     docs: list[dict],
     categories: list[str] = None,
