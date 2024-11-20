0b3afbe3afa32f1a8f239c8b9ec341e482dbc81d
Clément Doumouro <clement.doumouro@gmail.com>
2024-11-20 15:38:07 +0100

    chore: update Pyinstaller entry point to Java call

--- datashare_spacy_worker/__main__.py
+++ datashare_spacy_worker/__main__.py
@@ -1,18 +1,22 @@
-import os
-import sys
+from pathlib import Path
+from typing import Annotated
 
+import typer
 from icij_worker import WorkerBackend
 from icij_worker.backend import start_workers
 
 from datashare_spacy_worker.app import PYTHON_TASK_GROUP
 
-if __name__ == "__main__":
-    # TODO: it would be nicer to have a dedicated utils to hit the top level CLI
-    #  endpoint from Python main...
-    # TODO: we should factorize this as much as possible with the DOCKER entrypoint,
-    #  potentially the docker entry point should this main as entry point
-    n_workers = os.environ.get("N_PROCESSING_WORKERS", 1)
-    config_path = sys.argv[1]
+cli_app = typer.Typer()
+
+
+@cli_app.command()
+def main(
+    config_path: Annotated[Path, typer.Argument(help="Path to config file")],
+    n_workers: Annotated[
+        int, typer.Option("-n", "--n-workers", help="Number of NLP workers")
+    ] = 1,
+):
     start_workers(
         "datashare_spacy_worker.app.app",
         n_workers,
@@ -20,3 +24,7 @@ if __name__ == "__main__":
         backend=WorkerBackend.MULTIPROCESSING,
         group=PYTHON_TASK_GROUP,
     )
+
+
+if __name__ == "__main__":
+    cli_app()
